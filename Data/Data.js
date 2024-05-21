a234c094d966c218027bf554c9e19d77e67fd439
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-05-21 18:53:11 +0600

    Data

---
+++ Data/Data.js
@@ -0,0 +1,170 @@
+export const indiaData = [
+  {
+    id: 1,
+    name: "Mumbai",
+    description:
+      "Mumbai is the centre of the Mumbai Metropolitan Region, the sixth-most populous metropolitan area in the world with a population of over 23 million (2.3 crore).",
+    image:
+      "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcRIanR0TDpIJeypxtS4-bVRyKfq-5qfXkwcT1a3oLlS-wZMhHqF6XtZrDX71bNllOsL_3laxtVx0Mgk47wTcOy0EOxBr6ixATU3zwQT8w",
+    rating: "5.0",
+  },
+  {
+    id: 2,
+    name: "Goa",
+    description:
+      "Goa is a state on the southwestern coast of India within the Konkan region, geographically separated from the Deccan highlands by the Western Ghats.",
+    image:
+      "https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcTTHFEUHlVB8eTs432rFv94gdY7AmC41r5CLp0K3vfO_ypNITZniS9HowrCS1UicGV4YdzPxT96JGOdZPXgbz0FRpABXraRQRm33GNEMQ",
+    rating: "5.0",
+  },
+  {
+    id: 3,
+    name: "Kochi",
+    description:
+      "Today, Kochi has been described as the financial, commercial and industrial capital of Kerala. Kochi is the only city in the country to have a water metro .",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipOY40Kq58tqGHIBzBXM-a1JS8l3_F0krqRSCozf=w540-h312-n-k-no",
+    rating: "4.4",
+  },
+  {
+    id: 4,
+    name: "Kolkata",
+    description:
+      "Kolkata, also known as Calcutta is the capital and largest city of the Indian state of West Bengal. It lies on the eastern bank of the Hooghly River, ...",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipNsIVE7E2eQ_MI9RFNeE9i96IK8fTY8RtX41tDX=w540-h312-n-k-no",
+    rating: "4.9",
+  },
+  {
+    id: 5,
+    name: "Delhi",
+    description:
+      "Delhi, officially the National Capital Territory (NCT) of Delhi, is a city and a union territory of India containing New Delhi, the capital of India.",
+    image:
+      "https://lh5.googleusercontent.com/proxy/Wjn-MZbtcTiGNoeguVIHalSW8ACnEK8aQKGeg47MacSlJHLitMkIsyGghAR1ZNlh1A8mKYcOhhd51ZFwzU8EBxqByuIiGNoBLC4DFR68wHOhye6SHpXywLUZUtZzPQVScZwaSu2jHRvwdko0o6m66RxcstOFQG8=w540-h312-n-k-no",
+    rating: "4.5",
+  },
+  {
+    id: 6,
+    name: "Gujarat",
+    description:
+      "Gujarat was one of the main central areas of the Indus Valley civilisation, which is centred primarily in modern Pakistan. It contains ancient metropolitan cities from the Indus Valley",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipMU3k2ONQeR072h1iK4-68-NHSLWqkQMVrzmibJ=w540-h312-n-k-no",
+    rating: "5.0",
+  },
+  {
+    id: 7,
+    name: "Bangalore",
+    description:
+      "Bangalore is an anglicised version of the city's Kannada name Bengalūru. It was the name of a village near Kodigehalli in Bangalore city today and was used by Kempe Gowda to name the city as Bangalore at the time of its foundation in 1537.",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipPIRZTX1cUpsctw9N-I6WKLJxrjTza4D1v5HE_u=w540-h312-n-k-no",
+    rating: "5.0",
+  },
+  {
+    id: 8,
+    name: "Chennai",
+    description:
+      "Chennai formerly known as Madras, is the capital city of Tamil Nadu, the southernmost state of India. It is the state's primate city and is located on the",
+    image:
+      "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcSUi9RE4Zo7DfAZxY2NuDhGZliW6KlkGjvPFUXVkmH5InUwK6IE2wUjgZlwIhb1lsOZ1Adr-dhefQ2yMoKERGqL0HiSHM1KoWYyBtOQSPk",
+    rating: "4.8",
+  },
+];
+
+export const dubaiData = [
+  {
+    id: 1,
+    name: "Burj Khalifa",
+    description:
+      "Towering at a height of 2,700 feet, the Burj Khalifa is currently the tallest tower in the world. With two observation decks, a Las Vegas-inspired fountain",
+    image:
+      "https://www.holidify.com/images/cmsuploads/compressed/shutterstock_779374957_20190822125404_20190822125410.jpg",
+    rating: "5.0",
+  },
+  {
+    id: 2,
+    name: "Deira",
+    description:
+      "The area between the Dubai-Sharjah border and Dubai Creek is known as Deira. It is an area traditionally known for commercial activities with historical modes of transportation in Dhows being of particular interest to the tourists",
+    image:
+      "https://www.holidify.com/images/cmsuploads/compressed/shutterstock_294266150_20190904163902.jpg",
+    rating: "5.0",
+  },
+  {
+    id: 3,
+    name: "Bur Dubai",
+    description:
+      "Located near the Creek, the Bur Dubai, also known as Old Dubai, is a stark contrast from the Emirate's futuristic cityscape. It is the perfect way to get a peek into Dubai’s past culture, lifestyle, architecture and maritime history.",
+    image:
+      "https://www.holidify.com/images/cmsuploads/compressed/shutterstock_1244055283_20190822135309_20190822135326.jpg",
+    rating: "4.4",
+  },
+  {
+    id: 4,
+    name: "Mall of the Emirates",
+    description:
+      "The Mall of the Emirates, located in the Al Barsha area of West Dubai, is he 'World’s first shopping resort.' It has also been named the “World's Leading New Shopping Mall”",
+    image:
+      "https://www.holidify.com/images/cmsuploads/compressed/Mall_of_the_Emirates_20190414113024.jpg",
+    rating: "4.9",
+  },
+  {
+    id: 5,
+    name: "Dubai Fountain",
+    description:
+      "Dubai Fountain is the world's largest choreographed water fountain located in the beautiful Burj Complex.",
+    image:
+      "https://www.holidify.com/images/cmsuploads/compressed/shutterstock_1196820841_20190903122749.jpg",
+    rating: "4.5",
+  },
+  {
+    id: 6,
+    name: "Dubai Opera",
+    description:
+      "Located in Downtown Dubai, this 2016 established Opera is one of the finest and most radiant cultural and art centres.",
+    image:
+      "https://www.holidify.com/images/cmsuploads/compressed/36513663713_fb85946246_o_20180704000718.jpg",
+    rating: "5.0",
+  },
+];
+
+export const srilankaData = [
+  {
+    id: 1,
+    name: "Sigiriya",
+    description:
+      "It is likely that the area around Sigiriya was inhabited since prehistoric times.",
+    image:
+      "https://lh3.googleusercontent.com/p/AF1QipMS6ryNJjdQQNdAI0SZTT1j9vgRimN7iDtVgUm2=s680-w680-h510",
+    rating: "5.0",
+  },
+  {
+    id: 2,
+    name: "Sri Dalada Maligawa",
+    description:
+      "Sri Dalada Maligawa, commonly known in English as the Temple of the Sacred Tooth Relic, is a Buddhist temple in Kandy, Sri Lanka",
+    image:
+      "https://lh3.googleusercontent.com/p/AF1QipOeRfjUm-OwTpVJsqYiapIA3h0dVLob6T9WILIC=s680-w680-h510",
+    rating: "5.0",
+  },
+  {
+    id: 3,
+    name: "Galle Dutch Fort",
+    description:
+      "Galle Fort, in the Bay of Galle on the southwest coast of Sri Lanka, was built first in 1588 by the Portuguese, then extensively fortified by the Dutch during the 17th century from 1649 onwards",
+    image:
+      "https://lh3.googleusercontent.com/p/AF1QipObsU-GHqbTwIfzaz9cpBsDcRJ6G6QBKkQ04TI7=s680-w680-h510",
+    rating: "4.4",
+  },
+  {
+    id: 4,
+    name: "Golden Temple",
+    description:
+      "Dambulla cave temple, also known as the Golden Temple of Dambulla, is a World Heritage Site in Sri Lanka, situated in the central part of the country.",
+    image:
+      "https://lh3.googleusercontent.com/p/AF1QipPRdEwWOBSfRcchnYo83DCU38l4uXrpWNPi9_vn=s680-w680-h510",
+    rating: "4.9",
+  },
+];
